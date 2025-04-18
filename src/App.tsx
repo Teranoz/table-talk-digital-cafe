7ad694598c645cc011dcdfec65727df9675c2e65
Teranoz <85426239+Teranoz@users.noreply.github.com>
2025-04-18 23:26:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ScanPage from "./pages/ScanPage";
 import MenuPage from "./pages/MenuPage";
 import HelpPage from "./pages/HelpPage";
 import ReservationsPage from "./pages/ReservationsPage";
-import SupabaseTest from './pages/SupabaseTest'; // استيراد صفحة SupabaseTest
+import SupabaseTest from "./pages/SupabaseTest"; // تأكد أن الملف موجود في هذا المسار
 
 const queryClient = new QueryClient();
 
@@ -25,7 +26,7 @@ const App = () => (
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/help" element={<HelpPage />} />
           <Route path="/reservations" element={<ReservationsPage />} />
-          <Route path="/test" element={<SupabaseTest />} />  {/* إضافة المسار الجديد */}
+          <Route path="/test" element={<SupabaseTest />} /> {/* صفحة الاختبار */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
