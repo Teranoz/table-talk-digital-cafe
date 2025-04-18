e30498b54bc21d8e3950a7c09f7acc31b3a79d16
Teranoz <85426239+Teranoz@users.noreply.github.com>
2025-04-18 20:30:51 +0200

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-id.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://mpopoqnxydaigaxecuzf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3BvcW54eWRhaWdheGVjdXpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTkwMjcsImV4cCI6MjA2MDU3NTAyN30.7HnfN5uMU7TKC9uqvrWMoJnlBkvUoYxG-z6pNyGzV_U';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(https://mpopoqnxydaigaxecuzf.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wb3BvcW54eWRhaWdheGVjdXpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTkwMjcsImV4cCI6MjA2MDU3NTAyN30.7HnfN5uMU7TKC9uqvrWMoJnlBkvUoYxG-z6pNyGzV_U);
