bf4aec6c45393b93a00c7f93bcd4accd8628128f
Teranoz <85426239+Teranoz@users.noreply.github.com>
2025-04-19 16:55:48 +0200

    Create Register.tsx

---
+++ src/pages/Register.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { supabase } from "../supabase/client";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) alert(error.message);
+    else alert("Check your email for confirmation link");
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
