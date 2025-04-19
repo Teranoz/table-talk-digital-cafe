82566a0566d01d7f923a273bed9a1e64ffb99773
Teranoz <85426239+Teranoz@users.noreply.github.com>
2025-04-19 16:54:31 +0200

    Create Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { supabase } from "../supabase/client";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) alert(error.message);
+    else alert("Logged in!");
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
