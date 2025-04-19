87bcb45ebcecda05568166c4e45e0ec9ac353a53
Teranoz <85426239+Teranoz@users.noreply.github.com>
2025-04-20 00:10:16 +0200

    Create TestPage.tsx

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,14 @@
+// src/pages/TestPage.tsx
+
+import React from 'react';
+
+const TestPage: React.FC = () => {
+  return (
+    <div>
+      <h1>مرحبًا بك في صفحة الاختبار!</h1>
+      <p>هذه صفحة اختبار لعرض التوجيه في التطبيق.</p>
+    </div>
+  );
+};
+
+export default TestPage;
